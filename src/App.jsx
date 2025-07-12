906a4e7a52362efd9f48e0f526a650dff5adf483
Ashish035780 <150609098+Ashish035780@users.noreply.github.com>
2025-07-13 00:59:31 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,262 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Content1 from "./Component/Content1"
+import Job from "./Component/Job"
+import Logo from "./Component/Logo"
+import Logo1 from "./Component/Logo1"
+import Logo2 from "./Component/Logo2"
+import Nav from "./Component/Nav"
+import A00__Newjob from "./Component/Content/A00__Newjob"
+import Footer from "./Component/Footer"
+import A00_Admission_Form from "./Component/Content/A00_Admission_form"
+import A00_Admit_Card from "./Component/Content/A00_Admit_Card"
+import A00_Result from "./Component/Content/A00__Result"
+import A00_Answer_Key from "./Component/Content/A00_Answer_Key"
+import A00_Document_verification from "./Component/Content/A00_Document_verification"
+import A00_syllabus from "./Component/Content/A00_syllabus"
+import A001_Bank_job from "./Component/Content/A001_Bank_job"
+import A001_Defence_job from "./Component/Content/A001_Defence_job"
+import A001_Railway_job from "./Component/Content/A001_Railway_job"
+import A001_Ssc_job from "./Component/Content/A001_Ssc_job"
+import A001_Government_job from "./Component/Content/A001_Government_job"
+import Footer1 from "./Component/Footer1"
+import Animation from "./Component/Animation"
+import A0_Aadharcard from "./Component/Data/A0_Aadharcard"
+import A0_Voter_Id_Card from "./Component/Data/A0_Voter_Id_Card"
+import A0_Pan_Card from "./Component/Data/A0_Pan_Card"
+import A0_Bihar_RTPS from "./Component/Data/A0_Bihar_RTPS"
+import A0_UP_eDistrict from "./Component/Data/A0_UP_eDistrict"
+import A0_Privacy_Policy from "./Component/Data/A0_Privacy_Policy"
+import A0_Terms_Condition from "./Component/Data/A0_Terms_Condition"
+import A0_Disclaimer from "./Component/Data/A0_Disclaimer"
+import BPSC_1 from "./Component/Data/BPSC_1"
+import BPSC_2 from "./Component/Data/BPSC_2"
+import SBI_Clerk_1 from "./Component/Data/SBI_Clerk_1"
+import BPSC_TRE_1 from "./Component/Data/BPSC_TRE_1"
+import Railway_Assistant_Loco_Pilot_1 from "./Component/Data/Railway_Assistant_Loco_Pilot_1"
+import Rajasthan_TGT_Teacher_1 from "./Component/Data/Rajasthan_TGT_Teacher_1"
+import ContactUs from "./Component/ContactUs"
+import A00_Scholarship from "./Component/Content/A00_Scholarship"
+import A00_Sarkari_yojana from "./Component/Content/A00_Sarkari_Yojana"
+import Bihar_Police_Constable_1 from "./Component/Data/Bihar_Police_Constable_1"
+import Bihar_Police_Constable_2 from "./Component/Data/Bihar_Police_Constable_2"
+import SSC_GD_1 from "./Component/Data/SSC_GD_1"
+import SSC_MTS_1 from "./Component/Data/SSC_MTS_1"
+import SSC_Chsl_1 from "./Component/Data/SSC_Chsl_1"
+import SSC_CGL_1 from "./Component/Data/SSC_CGL_1"
+import BARC_1 from "./Component/Data/BARC_1"
+import Bihar_Police_bpssc_sub_inspector_1 from "./Component/Data/Bihar_Police_bpssc_sub_inspector_1"
+import Bihar_Polytechnic_1 from "./Component/Data/Bihar_Polytechnic_1"
+import Bihar_Polytechnic_2 from "./Component/Data/Bihar_Polytechnic_2"
+import BIHAR_STET_1 from "./Component/Data/BIHAR_STET_1"
+import BIHAR_STET_2 from "./Component/Data/BIHAR_STET_2"
+import AIIMS_NORCET_1 from "./Component/Data/AIIMS_NORCET_1"
+import AIIMS_NORCET_2 from "./Component/Data/AIIMS_NORCET_2"
+import Allahabad_HighCourt_Research_Associate_1 from "./Component/Data/Allahabad_HighCourt_Research_Associate_1"
+import Allahabad_HighCourt_Research_Associate_2 from "./Component/Data/Allahabad_HighCourt_Research_Associate_2"
+import Army_TGC_1 from "./Component/Data/Army_TGC_1"
+import Army_TGC_2 from "./Component/Data/Army_TGC_2"
+import Bihar_Police_bpssc_sub_inspector_2 from "./Component/Data/Bihar_Police_bpssc_sub_inspector_2"
+import BPSC_TRE_2 from "./Component/Data/BPSC_TRE_2"
+import CLAT_UG_PG_Entrance_1 from "./Component/Data/CLAT_UG_PG_Entrance_1"
+import CLAT_UG_PG_Entrance_2 from "./Component/Data/CLAT_UG_PG_Entrance_2"
+import BARC_2 from "./Component/Data/BARC_2"
+import CTET_1 from "./Component/Data/CTET_1"
+import CTET_2 from "./Component/Data/CTET_2"
+import IBPS_clerk_1 from "./Component/Data/IBPS_clerk_1"
+import IBPS_clerk_2 from "./Component/Data/IBPS_clerk_2"
+import IBPS_PO_MT_1 from "./Component/Data/IBPS_PO_MT_1"
+import IBPS_PO_MT_2 from "./Component/Data/IBPS_PO_MT_2"
+import IDBI_Bank_1 from "./Component/Data/IDBI_Bank_1"
+import IDBI_Bank_2 from "./Component/Data/IDBI_Bank_2"
+import Indian_Coast_Guard_GD_1 from "./Component/Data/Indian_Coast_Guard_GD_1"
+import Indian_Coast_Guard_GD_2 from "./Component/Data/Indian_Coast_Guard_GD_2"
+import IRDAI_Assistant_Manager_1 from "./Component/Data/IRDAI_Assistant_Manager_1"
+import IRDAI_Assistant_Manager_2 from "./Component/Data/IRDAI_Assistant_Manager_2"
+import ISRO_VSSC_1 from "./Component/Data/ISRO_VSSC_1"
+import ISRO_VSSC_2 from "./Component/Data/ISRO_VSSC_2"
+import MPESB_Nursing_1 from "./Component/Data/MPESB_Nursing_1"
+import MPESB_Nursing_2 from "./Component/Data/MPESB_Nursing_2"
+import MPPSC_1 from "./Component/Data/MPPSC_1"
+import MPPSC_2 from "./Component/Data/MPPSC_2"
+import Nabard_Assistant_Manager_1 from "./Component/Data/Nabard_Assistant_Manager_1"
+import Nabard_Assistant_Manager_2 from "./Component/Data/Nabard_Assistant_Manager_2"
+import NCERT_Non_Teaching_Recruitment_1 from "./Component/Data/NCERT_Non_Teaching_Recruitment_1"
+import NCERT_Non_Teaching_Recruitment_2 from "./Component/Data/NCERT_Non_Teaching_Recruitment_2"
+import NVS_Non_Teaching_1 from "./Component/Data/NVS_Non_Teaching_1"
+import NVS_Non_Teaching_2 from "./Component/Data/NVS_Non_Teaching_2"
+import Railway_Assistant_Loco_Pilot_2 from "./Component/Data/Railway_Assistant_Loco_Pilot_2"
+import Railway_Group_D_1 from "./Component/Data/Railway_Group_D_1"
+import Railway_Group_D_2 from "./Component/Data/Railway_Group_D_2"
+import Railway_RPF_1 from "./Component/Data/Railway_RPF_1"
+import Railway_RPF_2 from "./Component/Data/Railway_RPF_2"
+import Rajasthan_TGT_Teacher_2 from "./Component/Data/Rajasthan_TGT_Teacher_2"
+import SBI_Clerk_2 from "./Component/Data/SBI_Clerk_2"
+import SSC_CGL_2 from "./Component/Data/SSC_CGL_2"
+import SSC_Chsl_2 from "./Component/Data/SSC_Chsl_2"
+import SSC_GD_2 from "./Component/Data/SSC_GD_2"
+import SSC_MTS_2 from "./Component/Data/SSC_MTS_2"
+import UP_Police_Head_Operator_1 from "./Component/Data/UP_Police_Head_Operator_1"
+import UP_Police_Head_Operator_2 from "./Component/Data/UP_Police_Head_Operator_2"
+import UPPSC_ATP_1 from "./Component/Data/UPPSC_ATP_1"
+import UPPSC_ATP_2 from "./Component/Data/UPPSC_ATP_2"
+import UPPSC_Staff_Nurse_1 from "./Component/Data/UPPSC_Staff_Nurse_1"
+import UPPSC_Staff_Nurse_2 from "./Component/Data/UPPSC_Staff_Nurse_2"
+import UPSC_CDS_1 from "./Component/Data/UPSC_CDS_1"
+import UPSC_CDS_2 from "./Component/Data/UPSC_CDS_2"
+import UPSC_Civil_Services_1 from "./Component/Data/UPSC_Civil_Services_1"
+import UPSC_Civil_Services_2 from "./Component/Data/UPSC_Civil_Services_2"
+import UPSC_Combined_Medical_1 from "./Component/Data/UPSC_Combined_Medical_1"
+import UPSC_Combined_Medical_2 from "./Component/Data/UPSC_Combined_Medical_2"
+import UPSC_Engineering_Services_1 from "./Component/Data/UPSC_Engineering_Services_1"
+import UPSC_Engineering_Services_2 from "./Component/Data/UPSC_Engineering_Services_2"
+import UPSC_IES_ISS_1 from "./Component/Data/UPSC_IES_ISS_1"
+import UPSC_IES_ISS_2 from "./Component/Data/UPSC_IES_ISS_2"
+import UPSC_NDA_1 from "./Component/Data/UPSC_NDA_1"
+import UPSC_NDA_2 from "./Component/Data/UPSC_NDA_2"
+
+const App=()=>{
+    return(
+        <div>
+            
+            <BrowserRouter>
+            
+            <Logo/>
+            <Nav/>
+            <Routes><Route path="/" element={<Logo2/>}></Route></Routes>
+            
+            {/* <GoogleAdsense adClient="ca-pub-7825566122841628" adSlot="1707647920" adFormat="auto" fullWidthResponsive={true}/> */}
+            <Routes><Route path="/" element={<Animation/>} ></Route></Routes>
+            
+            <Routes><Route path="/" element={<Logo1/>} ></Route></Routes>
+            {/* <GoogleAdsense adClient="ca-pub-7825566122841628" adSlot="1707647920" adFormat="auto" fullWidthResponsive={true}/> */}
+            <Routes><Route path="/" element={<Job/>} ></Route></Routes>
+             
+            <Routes><Route path="/" element={<Content1/>} ></Route></Routes>
+            <Routes><Route path="/" element={<Footer1/>} ></Route></Routes>
+
+            {/* -------------------------Navigation--------------------------- */}
+
+            <Routes>
+                <Route path="/newjob" element={<A00__Newjob/>}></Route>
+                <Route path="/admitcard" element={<A00_Admit_Card/>}></Route>
+                <Route path="/result" element={<A00_Result/>}></Route>
+                <Route path="/answerkey" element={<A00_Answer_Key/>}></Route>
+                <Route path="/document" element={<A00_Document_verification/>}></Route>
+                <Route path="/admission" element={<A00_Admission_Form/>}></Route>
+                <Route path="/syllabus" element={<A00_syllabus/>}></Route>
+                <Route path="/scholarship" element={<A00_Scholarship/>}></Route>
+                <Route path="/sarkariyojana" element={<A00_Sarkari_yojana/>}></Route>
+                <Route path="/bankjob" element={<A001_Bank_job/>}></Route>
+                <Route path="/defencejob" element={<A001_Defence_job/>}></Route>
+                <Route path="/railwayjob" element={<A001_Railway_job/>}></Route>
+                <Route path="/sscjob" element={<A001_Ssc_job/>}></Route>
+                <Route path="/governmentjob" element={<A001_Government_job/>}></Route>
+                <Route path="/privacypolicy" element={<A0_Privacy_Policy/>}></Route>
+                <Route path="/termscondition" element={<A0_Terms_Condition/>}></Route>
+                <Route path="/disclaimer" element={<A0_Disclaimer/>}></Route>
+                <Route path="/contactus" element={<ContactUs/>}></Route>
+            </Routes>
+            
+            {/* ---------------------------Document Verification----------------------------- */}
+
+            <Routes>
+            <Route path="/aadharcard" element={<A0_Aadharcard/>}></Route>
+            <Route path="/voteridcard" element={<A0_Voter_Id_Card/>}></Route>
+            <Route path="/pancard" element={<A0_Pan_Card/>}></Route>
+            <Route path="/biharrtps" element={<A0_Bihar_RTPS/>}></Route>
+            <Route path="/upedistrict" element={<A0_UP_eDistrict/>}></Route>
+            </Routes>
+
+            {/* ----------------------------------Data-------------------------------------- */}
+
+            <Routes>
+            <Route path="/aiimsnorcet1" element={<AIIMS_NORCET_1 />}></Route>
+            <Route path="/aiimsnorcet2" element={<AIIMS_NORCET_2 />}></Route>
+            <Route path="/allahabadhighcourtresearchassociate1" element={<Allahabad_HighCourt_Research_Associate_1 />}></Route>
+            <Route path="/allahabadhighcourtresearchassociate2" element={<Allahabad_HighCourt_Research_Associate_2 />}></Route>
+            <Route path="/armytgc1" element={<Army_TGC_1 />}></Route>
+            <Route path="/armytgc2" element={<Army_TGC_2 />}></Route>
+            <Route path="/barc1" element={<BARC_1 />}></Route>
+            <Route path="/barc2" element={<BARC_2 />}></Route>
+            <Route path="/biharpolicebpsscsubinspector1" element={<Bihar_Police_bpssc_sub_inspector_1 />}></Route>
+            <Route path="/biharpolicebpsscsubinspector2" element={<Bihar_Police_bpssc_sub_inspector_2 />}></Route>
+            <Route path="/biharpoliceconstable1" element={<Bihar_Police_Constable_1 />}></Route>
+            <Route path="/biharpoliceconstable2" element={<Bihar_Police_Constable_2 />}></Route>
+            <Route path="/biharpolytechnic1" element={<Bihar_Polytechnic_1 />}></Route>
+            <Route path="/biharpolytechnic2" element={<Bihar_Polytechnic_2 />}></Route>
+            <Route path="/biharstet1" element={<BIHAR_STET_1/>}></Route>
+            <Route path="/biharstet2" element={<BIHAR_STET_2/>}></Route>
+            <Route path="/bpsc1" element={<BPSC_1 />}></Route>
+            <Route path="/bpsc2" element={<BPSC_2 />}></Route>
+            <Route path="/bpsctre1" element={<BPSC_TRE_1 />}></Route>
+            <Route path="/bpsctre2" element={<BPSC_TRE_2 />}></Route>
+            <Route path="/clatugpgentrance1" element={<CLAT_UG_PG_Entrance_1 />}></Route>
+            <Route path="/clatugpgentrance2" element={<CLAT_UG_PG_Entrance_2 />}></Route>
+            <Route path="/ctet1" element={<CTET_1 />}></Route>
+            <Route path="/ctet2" element={<CTET_2 />}></Route>
+            <Route path="/ibpsclerk1" element={<IBPS_clerk_1 />}></Route>
+            <Route path="/ibpsclerk2" element={<IBPS_clerk_2 />}></Route>
+            <Route path="/ibpspomt1" element={<IBPS_PO_MT_1 />}></Route>
+            <Route path="/ibpspomt2" element={<IBPS_PO_MT_2 />}></Route>
+            <Route path="/idbibank1" element={<IDBI_Bank_1 />}></Route>
+            <Route path="/idbibank2" element={<IDBI_Bank_2 />}></Route>
+            <Route path="/indiancoastguardgd1" element={<Indian_Coast_Guard_GD_1 />}></Route>
+            <Route path="/indiancoastguardgd2" element={<Indian_Coast_Guard_GD_2 />}></Route>
+            <Route path="/irdaiassistantmanager1" element={<IRDAI_Assistant_Manager_1 />}></Route>
+            <Route path="/irdaiassistantmanager2" element={<IRDAI_Assistant_Manager_2 />}></Route>
+            <Route path="/isrovssc1" element={<ISRO_VSSC_1 />}></Route>
+            <Route path="/isrovssc2" element={<ISRO_VSSC_2 />}></Route>
+            <Route path="/mpesbnursing1" element={<MPESB_Nursing_1 />}></Route>
+            <Route path="/mpesbnursing2" element={<MPESB_Nursing_2 />}></Route>
+            <Route path="/mppsc1" element={<MPPSC_1/>}></Route>
+            <Route path="/mppsc2" element={<MPPSC_2/>}></Route>
+            <Route path="/nabardassistantmanager1" element={<Nabard_Assistant_Manager_1 />}></Route>
+            <Route path="/nabardassistantmanager2" element={<Nabard_Assistant_Manager_2 />}></Route>
+            <Route path="/ncertnonteching1" element={<NCERT_Non_Teaching_Recruitment_1 />}></Route>
+            <Route path="/ncertnonteching2" element={<NCERT_Non_Teaching_Recruitment_2 />}></Route>
+            <Route path="/nvsnonteaching1" element={<NVS_Non_Teaching_1 />}></Route>
+            <Route path="/nvsnonteaching2" element={<NVS_Non_Teaching_2 />}></Route>
+            <Route path="/railwayassistantlocopilot1" element={<Railway_Assistant_Loco_Pilot_1 />}></Route>
+            <Route path="/railwayassistantlocopilot2" element={<Railway_Assistant_Loco_Pilot_2 />}></Route>
+            <Route path="/railwaygroupd1" element={<Railway_Group_D_1 />}></Route>
+            <Route path="/railwaygroupd2" element={<Railway_Group_D_2 />}></Route>
+            <Route path="/railwayrpf1" element={<Railway_RPF_1 />}></Route>
+            <Route path="/railwayrpf2" element={<Railway_RPF_2 />}></Route>
+            <Route path="/rajasthantgtteacher1" element={<Rajasthan_TGT_Teacher_1 />}></Route>
+            <Route path="/rajasthantgtteacher2" element={<Rajasthan_TGT_Teacher_2 />}></Route>
+            <Route path="/sbiclerk1" element={<SBI_Clerk_1 />}></Route>
+            <Route path="/sbiclerk2" element={<SBI_Clerk_2 />}></Route>
+            <Route path="/ssccgl1" element={<SSC_CGL_1 />}></Route>
+            <Route path="/ssccgl2" element={<SSC_CGL_2 />}></Route>
+            <Route path="/sscchsl1" element={<SSC_Chsl_1 />}></Route>
+            <Route path="/sscchsl2" element={<SSC_Chsl_2 />}></Route>
+            <Route path="/sscgd1" element={<SSC_GD_1 />}></Route>
+            <Route path="/sscgd2" element={<SSC_GD_2 />}></Route>
+            <Route path="/sscmts1" element={<SSC_MTS_1 />}></Route>
+            <Route path="/sscmts2" element={<SSC_MTS_2 />}></Route>
+            <Route path="/uppoliceheadoperator1" element={<UP_Police_Head_Operator_1 />}></Route>
+            <Route path="/uppoliceheadoperator2" element={<UP_Police_Head_Operator_2 />}></Route>
+            <Route path="/uppscatp1" element={<UPPSC_ATP_1 />}></Route>
+            <Route path="/uppscatp2" element={<UPPSC_ATP_2 />}></Route>
+            <Route path="/uppscstaffnurse1" element={<UPPSC_Staff_Nurse_1/>}></Route>
+            <Route path="/uppscstaffnurse2" element={<UPPSC_Staff_Nurse_2/>}></Route>
+            <Route path="/upsccds1" element={<UPSC_CDS_1 />}></Route>
+            <Route path="/upsccds2" element={<UPSC_CDS_2 />}></Route>
+            <Route path="/upsccivilservices1" element={<UPSC_Civil_Services_1 />}></Route>
+            <Route path="/upsccivilservices2" element={<UPSC_Civil_Services_2 />}></Route>
+            <Route path="/upsccombinedmedical1" element={<UPSC_Combined_Medical_1 />}></Route>
+            <Route path="/upsccombinedmedical2" element={<UPSC_Combined_Medical_2 />}></Route>
+            <Route path="/upscengineeringservices1" element={<UPSC_Engineering_Services_1 />}></Route>
+            <Route path="/upscengineeringservices2" element={<UPSC_Engineering_Services_2 />}></Route>
+            <Route path="/upsciesiss1" element={<UPSC_IES_ISS_1 />}></Route>
+            <Route path="/upsciesiss2" element={<UPSC_IES_ISS_2 />}></Route>
+            <Route path="/upscnda1" element={<UPSC_NDA_1 />}></Route>
+            <Route path="/upscnda2" element={<UPSC_NDA_2 />}></Route>
+            </Routes>
+            <Footer/>
+            </BrowserRouter>
+        </div>
+    )
+}
+
+export default App
